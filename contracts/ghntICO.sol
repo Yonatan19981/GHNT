c843d3ebf03202685e64ddf43c2ec691d92604b6
Yonatan Martsiano <Yonatan888@gmail.com>
2022-09-10 10:08:16 +0300

    Updated first audit notes

--- contracts/ghntICO.sol
+++ contracts/ghntICO.sol
@@ -5,17 +5,14 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./ghntVesting.sol";
-import "hardhat/console.sol";
+
 interface VestingContract{
  function withdraw(address _user) external;
 }
 
 
 contract ghntICO is ReentrancyGuard {
-
-    event Bought(uint amount, address vestingContract);
-
-    uint [] public vestingSchedule =[0, 0, 0, 0, 0, 20, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 0, 0, 0, 0, 0, 0, 0, 0];
+    uint [] public vestingSchedule =[10, 0, 0, 0, 0, 10, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 0, 0, 0, 0, 0, 0, 0, 0];
     mapping(address => address) public vested;
     address public ghntToken;
     uint public ratio;
@@ -32,17 +29,13 @@ contract ghntICO is ReentrancyGuard {
 
     }
 
-      function Buy() public payable nonReentrant() returns (address vesting){
-        console.log("balance : ",ERC20(ghntToken).balanceOf(address(this)));
-
+      function Buy() public payable nonReentrant() {
         require(vested[msg.sender]==address(0),"Wallet already bought");
         uint _amount=msg.value;
-        require(_amount>amount,"msg.value is less than minimum" );
-        console.log("_amount*ratio :",_amount*ratio);
+        require(_amount>0,"Cannot buy with 0 value of msg.value" );
         require(ERC20(ghntToken).balanceOf(address(this))>_amount*ratio,"Not enough ghnt tokens left in contract" );
-        vesting=vestInternal(msg.sender,ghntToken,_amount*ratio);
-        emit Bought(_amount*ratio,vested[msg.sender]);
-
+        vestInternal(msg.sender,ghntToken,_amount*ratio);
+        
     }
 
     function withdraw() public {
@@ -58,26 +51,16 @@ contract ghntICO is ReentrancyGuard {
      function withdrawAdmin(address _token,uint _amount) public {
         require(msg.sender==admin,"Only admin can withdraw");
         require(_token!=address(0),"Cannot insert zero address");
-        ERC20(_token).transferFrom(address(this),admin,_amount);
+        ERC20(_token).transfer(admin,_amount);
 
     }
-  function vestInternal(address _user,address _token,uint _amount) internal returns (address _vesting){
+  function vestInternal(address _user,address _token,uint _amount) internal {
     ghntVesting vesting=new ghntVesting(_user,vestingSchedule,vestingSchedule.length,_token,address(this),_amount);
     vested[_user]=address(vesting);
-    ERC20(_token).transfer(vested[_user],_amount);
-    _vesting=vested[_user];
     }
-
-        function changeAdmin(address _admin) public {
-        require(msg.sender==admin,"Only admin can change admins");
-        require(_admin!=address(0),"Cannot insert zero address");
-
-            admin=_admin;
-
-    }
-  
 
     }
   
     
+
 
